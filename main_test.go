13a90762456a3e39799a3c414f4ca6fa84523a40
Maksym Prokopov <maksym.prokopov@billie.io>
2026-05-22 15:01:01 +0200

    Remove Description field check from tests
    Removed test for Description field in main_test.go

--- main_test.go
+++ main_test.go
@@ -8,9 +8,6 @@ func TestSimpleFactory(t *testing.T) {
 	if got.Name != "Hello" {
 		t.Errorf("Name = %q, want %q", got.Name, "Hello")
 	}
-	if got.Description != "World" {
-		t.Errorf("Description = %q, want %q", got.Description, "World1")
-	}
 	if got.Url != "example.com:4444" {
 		t.Errorf("Url = %q, want %q", got.Url, "example.com:4444")
 	}
